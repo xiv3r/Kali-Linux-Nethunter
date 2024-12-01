4787bd7f795b28daac6abd9822c648021844a4e0
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-02 05:55:55 +0800

    Update kali-full.sh

--- kali/kali-full.sh
+++ kali/kali-full.sh
@@ -1,3 +1,3 @@
 DISTRO_NAME="Kali Nethunter Full"
-TARBALL_URL['aarch64']=""
+TARBALL_URL['aarch64']="https://github.com/xiv3r/Kali-Linux-Nethunter/releases/download/rootfs/kali-nethunter-full-rootfs-arm64.tar.xz"
 TARBALL_SHA256['aarch64']=""
