94220622a551a9796874b7b869c22efd12c330f5
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-02 05:53:45 +0800

    Update kali-minimal.sh

--- kali/kali-minimal.sh
+++ kali/kali-minimal.sh
@@ -1,3 +1,3 @@
 DISTRO_NAME="Kali Nethunter Minimal"
-TARBALL_URL['aarch64']=""
+TARBALL_URL['aarch64']="https://github.com/xiv3r/Kali-Linux-Nethunter/releases/download/rootfs/kali-nethunter-minimal-rootfs-arm64.tar.xz"
 TARBALL_SHA256['aarch64']="544d4d9459b8d9d1da680d05cd35a5ca668ca5e2f134e5c68a4ec97117fe821b"
