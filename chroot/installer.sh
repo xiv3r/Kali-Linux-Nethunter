6175485a31608a8e2c938d74e3de6332083749f1
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-02 08:04:58 +0800

    Update installer.sh

--- chroot/installer.sh
+++ chroot/installer.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+###
 wget https://kali.download/nethunter-images/current/rootfs/kali-nethunter-rootfs-minimal-arm64.tar.xz
+###
+wget -O $PREFIX/bin/nethunter https://raw.githubusercontent.com/xiv3r/Kali-Linux-Nethunter/refs/heads/main/chroot/nethunter
+###
+chmod 700 $PREFIX/bin/nethunter
+###
+tar -xf kali-nethunter-rootfs-minimal-arm64.tar.xz
