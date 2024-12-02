6a64ab7253970828d09096fd35a851781c8c3af5
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-03 05:51:43 +0800

    Update install.sh

--- kali/install.sh
+++ kali/install.sh
@@ -2,16 +2,17 @@
 
 cd $PREFIX/etc/proot-distro/
 ###
-wget https://raw.githubusercontent.com/xiv3r/Kali-Linux-Nethunter/refs/heads/main/kali/kali-full.sh
+# wget https://raw.githubusercontent.com/xiv3r/Kali-Linux-Nethunter/refs/heads/main/kali/kali-full.sh
 ###
 wget https://raw.githubusercontent.com/xiv3r/Kali-Linux-Nethunter/refs/heads/main/kali/kali-minimal.sh
 ###
 proot-distro list
 ###
 proot-distro install kali-minimal
-#echo "
-# To install e.g: proot-distro install kali-minimal     
-#"
+##$
+echo "
+To install e.g: proot-distro install kali-minimal     
+"
 ###
-# read -p "Enter a command: " command
-# eval "$command"
+read -p "Enter a command: " command
+eval "$command"
