f4da69e42969035ebc570386e852248b4e573561
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-03 05:45:57 +0800

    Update installer.sh

--- blackarch/installer.sh
+++ blackarch/installer.sh
@@ -1,1 +1,22 @@
 #!/bin/sh
+
+apt install proot proot-distro wget -y
+###
+echo 'DISTRO_NAME="BlackArch Linux"' >> $PREFIX/etc/proot-distro/blackarch.sh
+###
+pd list
+###
+cd $PREFIX/var/lib/proot-distro/installed-rootfs/
+###
+mkdir -p blackarch
+###
+wget https://github.com/xiv3r/Kali-Linux-Nethunter/releases/download/rootfs/blackarch-rootfs-arm64.tar.xz
+###
+xz -tf blackarch-rootfs-arm64.tar.xz
+###
+tar -xvf blackarch-rootfs-arm64.tar.xz -C blackarch/
+###
+echo "
+To login BackBox: pd sh blackarch
+"
+pd sh blackarch
