f2da4a1a30492527fe287ad36bb3f23617a0887f
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-03 05:11:23 +0800

    Update installer.sh

--- backbox/installer.sh
+++ backbox/installer.sh
@@ -2,12 +2,19 @@
 
 apt install proot proot-distro wget -y
 ###
-mkdir -p $PREFIX/val/lib/proot-distro/installed-rootfs/backbox
+echo 'DISTRO_NAME="BackBox"' >> $PREFIX/etc/proot-distro/bacbox.sh
+###
+pd list
 ###
 cd $PREFIX/val/lib/proot-distro/installed-rootfs/
 ###
+mkdir -p backbox
+###
 wget https://github.com/xiv3r/Kali-Linux-Nethunter/releases/download/rootfs/backbox-arm64-rootfs.tat.xz
 ###
-tar -xvf backbox-arm64-rootfs.tat.xz -C backbox
+tar -xvf backbox-arm64-rootfs.tat.xz -C backbox/
 ###
+echo "
+To login BackBox: pd sh backbox
+"
 pd sh backbox
