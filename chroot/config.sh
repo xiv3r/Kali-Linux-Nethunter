0c5874b6ef439d0412cec007f329a404b9ca47ab
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-02 13:10:42 +0800

    Update config.sh

--- chroot/config.sh
+++ chroot/config.sh
@@ -4,7 +4,7 @@ usermod -aG sudo kali
 ###
 echo "kali    ALL=(ALL:ALL) ALL" >>/etc/sudoers
 ###
-wget -O .bashrc https://raw.githubusercontent.com/xiv3r/Kali-Linux-Nethunter/refs/heads/main/chroot/rootbash.bashrc
+wget -O /root/.bashrc https://raw.githubusercontent.com/xiv3r/Kali-Linux-Nethunter/refs/heads/main/chroot/rootbash.bashrc
 ###
 wget -O /etc/bash.bashrc https://raw.githubusercontent.com/xiv3r/Kali-Linux-Nethunter/refs/heads/main/chroot/etcbash.bashrc
 ###
