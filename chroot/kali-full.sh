ab1d345f3b581e4b49a096fe7070c8335aedddd1
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-02 12:57:11 +0800

    Create kali-full.sh

---
+++ chroot/kali-full.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+apt install proot
+###
+wget https://kali.download/nethunter-images/current/rootfs/kali-nethunter-rootfs-full-arm64.tar.xz
+###
+wget -O $PREFIX/bin/nethunter https://raw.githubusercontent.com/xiv3r/Kali-Linux-Nethunter/refs/heads/main/chroot/nethunter
+###
+chmod 700 $PREFIX/bin/nethunter
+###
+echo "
+checking for tarball errors...
+"
+###
+xz -t kali-nethunter-rootfs-full-arm64.tar.xz
+###
+echo "
+Done checking!
+"
+###
+echo "
+extracting rootfs data...
+"
+###
+tar -xf kali-nethunter-rootfs-full-arm64.tar.xz
+###
+echo "
+errors doesn't affect root file system
+"
+###
+sleep 1s
+###
+echo "
+To Login
+user terminal: nethunter
+root terminal: nethunter -r
+"
