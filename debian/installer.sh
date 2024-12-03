13fec1a8eae2b1d0aed7b9976a26756a9c0befa9
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-03 10:38:06 +0800

    Update installer.sh

--- debian/installer.sh
+++ debian/installer.sh
@@ -1,1 +1,22 @@
 #!/bin/sh
+
+echo 'DISTRO_NAME="Debian Kali"' >> $PREFIX/etc/proot-distro/debkali.sh
+###
+pd list
+###
+mkdir -p $PREFIX/var/lib/proot-distro/installed-rootfs/debkali
+###
+cd $PREFIX/var/lib/proot-distro/installed-rootfs/
+###
+wget https://github.com/xiv3r/Kali-Linux-Nethunter/releases/download/rootfs/debkali-arm64-rootfs.tar.xz
+###
+tar -xvf debkali-arm64-rootfs.tar.xz -C debkali/
+###
+mv -f debkali/debian/* $PREFIX/var/lib/proot-distro/installed-rootfs/debkali
+###
+rm -rf debkali/debian
+###
+echo "
+To login Parrot Sec: pd sh parrot
+"
+pd sh debkali
